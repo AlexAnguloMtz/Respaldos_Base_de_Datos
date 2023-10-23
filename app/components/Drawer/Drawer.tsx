e9f0ca47de5034d2be51f10053dfc2267d3af27f
alex <raulalexo100@gmail.com>
2023-10-22 20:16:42 -0700

    Added close button to drawer

--- app/components/Drawer/Drawer.tsx
+++ app/components/Drawer/Drawer.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
 import styles from './styles.module.css';
 import Overlay from '../Overlay';
+import Image from 'next/image';
+import xMark from '../../../public/icons/xmark.svg';
 
 type Props = {
   active: boolean,
@@ -12,9 +14,27 @@ export const Drawer = ({ active, onClose }: Props): ReactNode => {
     <Overlay
       active={active}
       onClick={onClose}>
-      <div className={`${styles.drawer} ${active ? styles.active : ''}`}>
-        <h1>hello drawer</h1>
+      <div
+        className={`${styles.drawer} ${active ? styles.active : ''}`}
+        onClick={(e) => e.stopPropagation()}>
+        <div className={styles.closeButtonContainer}>
+          <CloseButton onClick={onClose} />
+        </div>
       </div>
     </Overlay>
   );
-} 
+}
+
+const CloseButton = ({ onClick }: { onClick: () => void }): ReactNode => {
+  return (
+    <button
+      className={styles.closeButton}
+      onClick={onClick}>
+      <Image
+        src={xMark}
+        alt={'close'}
+        width={25}
+        height={25} />
+    </button>
+  );
+}
