7eb882f059a64a69328c69a041771c106a8d5ca4
alex <raulalexo100@gmail.com>
2023-10-23 17:19:42 -0700

    fetching from backend is successful for home page

--- app/api/databases/route.ts
+++ app/api/databases/route.ts
@@ -1,21 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(
-    [
-      {
-        dbms: 'PostgreSQL',
-        version: '17.0',
-        users: ['luis', 'pedro'],
-        schemas: [
-          {
-            name: 'restaurante',
-            tables: [
-              'proveedores', 'clientes'
-            ]
-          }
-        ]
-      }
-    ]
-  )
+  const response: Response = await fetch('http://localhost:8080/databases');
+  return NextResponse.json(await response.json());
 }
